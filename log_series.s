8e9b9fe05fca6e2ee58956ed88a3a9b2570467ac
Aditya9630 <72200054+Aditya9630@users.noreply.github.com>
2020-10-23 13:27:56 +0530

    Update log_series.s

--- log_series.s
+++ log_series.s
@@ -4,28 +4,30 @@
 __main  function		 
               VLDR.f32 S3, = 1;
        VLDR.f32 S11, =0;	   
-       VLDR.f32 S10,= 0 ; R10 --->FINAL ANSWER
-       VLDR.f32 S5,= 0.7;
-       VMOV.f32 S8,S5; R5----> x
-       VLDR.f32 S2,= 0; R2----> SIGN
+       VLDR.f32 S10,= 0 ; S10 --->FINAL ANSWER
+       VLDR.f32 S14,= 0.7; ---> x
+       VMOV.f32 S8,S14; S8 represents term value initially x
+       VMOV.f32 S4,S3;  S4 represents Term  number initially its 1
+       VLDR.f32 S2,= 0; S2----> SIGN
        VLDR.f32  S9,= 1 ;   
-T1 		VMOV.f32 S9,S5 ; R9--->COUNT
-		VMOV.f32 S8,S5 
+T1 		VMOV.f32 S9,S4 ; R9--->Count power of x
+		VMOV.f32 S8,S14 
 		VCMP.f32 S9,S3 ;
-		VMRS APSR_nzcv,FPSCR
-    ITTE GT
-		VMULGT.F32 S8,S5,S8;
+T3		VMRS APSR_nzcv,FPSCR
+		BLE T2
+		VMULGT.F32 S8,S14,S8;
 		VSUBGT.F32 S9,S9,S3;
-
-		VDIVLE.F32 S8,S8,S5;
+		BGT  
+T2		VDIVLE.F32 S8,S8,S4; dividing by term no i.e x^n/n
 		VCMPLE.F32 S2,#0 ; R2---->SIGN
 		VMRS APSR_nzcv,FPSCR
 		ITTE EQ;
-		VADDEQ.f32 S10,S8,S10;
-		VMOVEQ.f32 S2,S3;
-		VSUBNE.F32 S10,S8,S10;
-		VMOVNE.f32 S2,S10;
-		VADD.F32 S5,S5,S3;
+		VADDEQ.f32 S10,S8,S10; add if sign in 0
+		VMOVEQ.f32 S2,S3; change sign to 1
+		VSUBNE.F32 S10,S10,S8;sub if sign is 1
+		VMOVNE.f32 S2,S11;change sign to 0
+		VADD.F32 S4,S4,S3;incrementing the term 
 		B T1 ;
      endfunc
      end
+;its a infinite series variefied upto few terms for x=0.7
